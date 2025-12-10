3e2c5ecac71bf75416dfad1d51018bed89ecc100
RedBearAK <64876997+RedBearAK@users.noreply.github.com>
2025-12-10 12:04:36 -0900

    Reduce minimum post-key throttle delay to 1ms

--- src/xwaykeyz/output.py
+++ src/xwaykeyz/output.py
@@ -79,7 +79,7 @@ def setup_uinput(uinput=None):
 
 # Throttle delay minimums defined here are calculated to hopefully
 # avoid interfering with most human-achievable normal typing speeds,
-# minimizing the risk of causing input buffering while maintaining
+# minimizing the risk of causing input buffering, while maintaining
 # some minimal temporal separation between all keystroke events,
 # even in the case of the user having their main throttle delay
 # values set to zero in their config. Or missing, defaulting to
@@ -87,8 +87,7 @@ def setup_uinput(uinput=None):
 # too many problems, may be partly responsible for some of the
 # "stuck key" issues some users run into.
 _THROTTLE_MIN_PRE_MS = 1
-_THROTTLE_MIN_POST_MS = 2
-
+_THROTTLE_MIN_POST_MS = 1
 
 class Output:
     def __init__(self):
